8caee1ecc08a181f9a2ca86bd098737f72cc4da4
MurituMaina <vincent.maina@student.moringaschool.com>
2022-09-15 11:54:37 +0300

    commit tree 6889bf7888505df12d886d7de8523859110704d6

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // lowercase the letters to be matchin
+  // remove any spaces or letters if any
+  //split the string 
+  //reverse and store it to a variable
+  // check if reverse value is equal (===) to the string
+  // returns true;
+  // let re = /[\W_]/g; // or 
+  // var re = /[^A-Za-z0-9]/g;
+let symbols = /[^A-Za-z0-9]/g;
+  let lowerCase = word.toLowerCase().replace(symbols, '');
+  
+  let stringReverse = lowerCase.split('').reverse().join(''); 
+
+if(stringReverse === lowerCase){
+  return true;
+}
+else{
+  return false
+}
+
 }
 
 /* 
   Add your pseudocode here
+  let symbols = /[A-Za-z0-9^]/g;
+  let lowerCase = word.toLowerCase().replace(symbols, '');
+  let stringReverse = lowerCase.split("").reverse().join("");
+if(stringReverse === lowerCase){
+  return true;
+}
+else{
+  return false
+}
 */
 
 /*
   Add written explanation of your solution here
+Converting all letters to lowercase allows for the letters to be uniform incase they were input in mixed 
+upper and lowercase.
+Removing the sysmbols and spaces to ensure no symbol will be checked as letter to avoid conflict when reversed.
+Re
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +54,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+    console.log("Expecting: false");
+  console.log("=>", isPalindrome("teacher"));
 }
 
 module.exports = isPalindrome;
